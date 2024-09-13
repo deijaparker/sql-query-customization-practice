9c22ba74f4ad992c47c67abbf9feb971a374f33c
deijaparker <deijaparker1@outlook.com>
2024-09-13 14:26:44 -0700

    Added SELECT and AS clause for renaming columns

---
+++ sql-query-practice.sql
@@ -0,0 +1,9 @@
+-- SQL Query Customization Example
+
+-- 1. SELECT and AS (renaming columns)
+SELECT 
+    product_id AS "Product ID", 
+    product_name AS "Product Name", 
+    price AS "Price"
+FROM 
+    products;
